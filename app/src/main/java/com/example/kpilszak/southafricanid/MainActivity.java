033c07f48be22e23d06ddac67a2042930d813e17
kpilszak <klaudia.pilszak@gmail.com>
2018-12-09 17:37:39 +0100

    Add btnSubmit onClickListener

--- app/src/main/java/com/example/kpilszak/southafricanid/MainActivity.java
+++ app/src/main/java/com/example/kpilszak/southafricanid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kpilszak.southafricanid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,5 +21,33 @@ public class MainActivity extends AppCompatActivity {
         etID = findViewById(R.id.etID);
         btnSubmit = findViewById(R.id.btnSubmit);
         tvResults = findViewById(R.id.tvResults);
+        tvResults.setVisibility(View.GONE);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String idNumber = etID.getText().toString().trim();
+                String dateOfBirth = idNumber.substring(0, 6);
+                int genderNumber = Integer.parseInt(Character.toString(idNumber.charAt(6)));
+                String sGender;
+                if (genderNumber < 5){
+                    sGender = getString(R.string.female);
+                } else {
+                    sGender = getString(R.string.male);
+                }
+                int nationalityNumber = Integer.parseInt(Character.toString(idNumber.charAt(10)));
+                String sNationality;
+                if (nationalityNumber == 0) {
+                    sNationality = getString(R.string.SACitizen);
+                } else {
+                    sNationality = getString(R.string.permanentResident);
+                }
+                String textResult = "\n" + getString(R.string.dateOfBirth) + dateOfBirth + "\n\n" +
+                        getString(R.string.gender) + sGender + "\n\n" +
+                        getString(R.string.nationality) + sNationality;
+                tvResults.setText(textResult);
+                tvResults.setVisibility(View.VISIBLE);
+           }
+        });
     }
 }
