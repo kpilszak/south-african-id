90ceadece6bc7ab41e89e2752612201c5efe1168
kpilszak <klaudia.pilszak@gmail.com>
2018-12-09 17:15:55 +0100

    Declare layout attributes

--- app/src/main/java/com/example/kpilszak/southafricanid/MainActivity.java
+++ app/src/main/java/com/example/kpilszak/southafricanid/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.kpilszak.southafricanid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etID;
+    Button btnSubmit;
+    TextView tvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
